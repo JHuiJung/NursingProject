1c5128759d79cf90332db07aa246a96371e0a17c
JHuiJung <wjdgmlwnd12@naver.com>
2025-07-27 21:42:38 +0900

    간호 기록지 제작 중

---
+++ Assets/Scripts/Simulation_Paper/Simulation_Paper.cs
@@ -0,0 +1,105 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+using UnityEngine;
+
+public class Simulation_Paper : SimulationBase
+{
+    [Header("Canvas Obj & Stuff"), Space(10)]
+    public GameObject Obj_CanvasChoice;
+
+
+    [TextArea] //질문
+    [Header("질문(필수로 입력)"), Space(10)]
+    public string text_Question = "";
+    public TMP_Text Tmp_Question;
+
+    [Header("Dotween"), Space(10)]
+    public float DG_Time = 0.75f;
+    public Ease DG_Ease = Ease.InOutQuad;
+
+    public bool isSimulationEnd = false;
+    private ScenarioManager _sm;
+
+    public override void Enter(ScenarioManager SM)
+    {
+        print($"{name} : 객관식 문제 시작");
+
+        Obj_CanvasChoice.SetActive(true);
+        _sm = SM;
+
+        Setup();
+        StartCoroutine(AllUiOn());
+    }
+
+    public override void Excute(ScenarioManager SM)
+    {
+        if (isSimulationEnd) return;
+    }
+
+    public override void Exit(ScenarioManager SM)
+    {
+        print($"{name} : 객관식 문제 끝");
+        ResetSimulation();
+        Obj_CanvasChoice.SetActive(false);
+    }
+    public override void ResetSimulation()
+    {
+        isSimulationEnd = false;
+    }
+
+    //------------------------------------------------------------------------------------------
+
+    void Setup()
+    {
+        // 질문 텍스트 수정
+        Tmp_Question.text = text_Question;
+
+        
+    }
+
+    IEnumerator AllUiOn()
+    {
+        // 타이틀 DG
+        RectTransform rect_title = Tmp_Question.gameObject.transform.parent
+            .GetComponent<RectTransform>();
+
+
+        rect_title.DOAnchorPos(new Vector2(rect_title.anchoredPosition.x,
+            0f), DG_Time).SetEase(DG_Ease);
+
+        yield return new WaitForSeconds(DG_Time);
+    }
+
+    IEnumerator AllUiOff()
+    {
+        // 타이틀 DG
+        RectTransform rect_title = Tmp_Question.gameObject.transform.parent
+            .GetComponent<RectTransform>();
+
+
+        rect_title.DOAnchorPos(new Vector2(rect_title.anchoredPosition.x,
+            200f), DG_Time).SetEase(DG_Ease);
+
+
+        yield return new WaitForSeconds(DG_Time);
+
+        // 다음 시뮬레이션으로 이동
+        _sm.NextSimulation();
+    }
+
+
+    public virtual void SubmitAnswer(string answer, int choosedNum)
+    {
+        isSimulationEnd = true;
+
+        _sm.str_Answers.Push($"{text_Question} / User Answer : {answer}");
+
+
+        // DG UI OFF
+        StartCoroutine(AllUiOff());
+    }
+
+}
