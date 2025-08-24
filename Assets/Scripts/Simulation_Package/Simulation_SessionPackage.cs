f5441fdedac2c0243506de1a1b1b40dd9a0c9aa5
JHuiJung <wjdgmlwnd12@naver.com>
2025-08-24 18:54:04 +0900

    시간 제한 기능 구현
    정해진 시간 초과시 처음부터 다시 시작하는 기능 추가

--- Assets/Scripts/Simulation_Package/Simulation_SessionPackage.cs
+++ Assets/Scripts/Simulation_Package/Simulation_SessionPackage.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Simulation_SessionPackage : SimulationBase
 {
@@ -10,11 +11,24 @@ public class Simulation_SessionPackage : SimulationBase
     //플레이어 이동 경로
     public List<Vector3> playerMovePositions = new List<Vector3>();
 
+    [Header("Time Check")]
+    public int timeThreshold = 180; // 몇초 후에 타임오버
+    public TMP_Text txt_TimeLeft;
+    public GameObject Obj_Area_TimeOut;
+
+    [Header("Reset Objects")]
+    public List<Objs_SetupForm> objs_SetupForms = new List<Objs_SetupForm>();
+
+    float _time = 0f;
     ScenarioManager _sm;
+    private Coroutine timeCheckRoutine;
 
     public override void Enter(ScenarioManager SM)
     {
         _sm = SM;
+
+        _time = 0f;
+
         // 자식 오브젝트 한개씩 생성해 붙이기
         LoadSimulations();
 
@@ -36,7 +50,7 @@ public class Simulation_SessionPackage : SimulationBase
 
     public override void ResetSimulation()
     {
-
+        _time = 0f;
     }
 
     void LoadSimulations()
@@ -46,12 +60,27 @@ public class Simulation_SessionPackage : SimulationBase
             SimulationBase simulationBase = transform.GetChild(i).GetComponent<SimulationBase>();
 
             _sm.simulationBases.Add(simulationBase);
+        }
+    }
 
-            //GameObject child = transform.GetChild(i).gameObject;
+    public void ResetObjs()
+    {
+        CameraManager.inst.SetCamera("Player");
 
-            //var Obj_Simulation = Instantiate(child,_sm.transform);
-            //Obj_Simulation.name = child.name;
-            //Obj_Simulation.transform.SetAsLastSibling();
+        foreach(Objs_SetupForm obj in objs_SetupForms)
+        {
+            GameObject targetObj = CameraManager.inst.GetGameObject(obj.obj_name);
+
+            if (targetObj != null)
+            {
+                targetObj.transform.position = obj.obj_position;
+                targetObj.transform.eulerAngles = obj.obj_rotation;
+                targetObj.SetActive(obj.is_active);
+            }
+            else
+            {
+                Debug.Log($"[Simulation_SessionPackage] : {obj.obj_name} not found in the scene.");
+            }
         }
     }
 
@@ -80,4 +109,58 @@ public class Simulation_SessionPackage : SimulationBase
     {
                _sm.NextSimulation();
     }
+
+    public void Start_TimeCheck()
+    {
+        timeCheckRoutine = StartCoroutine(Co_Start_TimeCheck());
+    }
+
+    public IEnumerator Co_Start_TimeCheck()
+    {
+        _time = 0f;
+
+        while (_time < timeThreshold)
+        {
+            // 1초 단위로 갱신
+            yield return new WaitForSeconds(1f);
+
+            _time += 1f;
+            int timeLeft = timeThreshold - Mathf.FloorToInt(_time);
+
+            txt_TimeLeft.text = $"남은 시간 : {timeLeft}초";
+        }
+
+        // 시간이 다 됐을 때 처리
+        txt_TimeLeft.text = "시간 종료!";
+
+        Obj_Area_TimeOut.SetActive(true);
+        
+    }
+
+
+    public void Stop_TimeCheck()
+    {
+        if (timeCheckRoutine != null)
+        {
+            StopCoroutine(timeCheckRoutine);
+            timeCheckRoutine = null;
+        }
+
+        _time = 0f;
+        txt_TimeLeft.text = $"";
+    }
+
+    public void ReturnSimultion()
+    {
+        _sm.ReturnSimultion();
+    }
+}
+
+[System.Serializable]
+public class Objs_SetupForm
+{
+    public string obj_name = "";
+    public Vector3 obj_position = Vector3.zero;
+    public Vector3 obj_rotation = Vector3.zero;
+    public bool is_active = true;
 }
